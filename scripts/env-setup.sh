2a625cdf860711c3f8f86bf481832832fc1203fd
Alexander Smolyakov <alexander.smolyakov.dev@gmail.com>
2022-12-22 17:58:50 +0400

    Add env-setup script

---
+++ scripts/env-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DBT_INVOCATION_ENV= "github-actions"
+export DBT_TEST_USER_1= "dbt_test_user_1"
+export DBT_TEST_USER_2= "dbt_test_user_2"
+export DBT_TEST_USER_3= "dbt_test_user_3"
