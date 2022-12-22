a2f45ebbbe11266512e1aff8d9343b58394181fd
Alexander Smolyakov <alexander.smolyakov.dev@gmail.com>
2022-12-22 19:06:48 +0400

    Update env-setup.sh

--- scripts/env-setup.sh
+++ scripts/env-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DBT_INVOCATION_ENV="github-actions"
-export DBT_TEST_USER_1="dbt_test_user_1"
-export DBT_TEST_USER_2="dbt_test_user_2"
-export DBT_TEST_USER_3="dbt_test_user_3"
+echo "DBT_INVOCATION_ENV=github-actions" >> $GITHUB_ENV
+echo "DBT_TEST_USER_1=dbt_test_user_1" >> $GITHUB_ENV
+echo "DBT_TEST_USER_2=dbt_test_user_2" >> $GITHUB_ENV
+echo "DBT_TEST_USER_3=dbt_test_user_3" >> $GITHUB_ENV
