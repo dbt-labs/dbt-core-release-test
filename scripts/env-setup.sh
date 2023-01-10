5fe738fbbe723878c6ff639e4978f8945363ac8b
Alexander Smolyakov <alexander.smolyakov.dev@gmail.com>
2023-01-10 13:47:05 +0400

    [Release] Cumulative update  (#21)
    - Introduce the "env_setup_script_path" input;
    - Add "env-setup.sh" to setup environment for integration tests;
    - Replace "gh-action-pypi-publish" GH action with PyPI Release reusable workflow;
    - Add "slack-notification" stage;
    - Replace "GITHUB_TOKEN" with "FISHTOWN_BOT_PAT";

---
+++ scripts/env-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "DBT_INVOCATION_ENV=github-actions" >> $GITHUB_ENV
+echo "DBT_TEST_USER_1=dbt_test_user_1" >> $GITHUB_ENV
+echo "DBT_TEST_USER_2=dbt_test_user_2" >> $GITHUB_ENV
+echo "DBT_TEST_USER_3=dbt_test_user_3" >> $GITHUB_ENV
